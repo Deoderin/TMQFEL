fa01fde40d86ebf3d0a967a70f9037c3edd96c5c
Deoderin <Deoderin@gmail.com>
2026-04-05 22:41:50 +0300

    Added initial scene

---
+++ Assets/Core/Scripts/Core/InitialPoint.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TMQFEL.Core
+{
+    [DefaultExecutionOrder(-1000)]
+    [DisallowMultipleComponent]
+    public sealed class InitialPoint : MonoBehaviour
+    {
+        [SerializeField] private string initialSceneName = "GameScene";
+
+        private void Start()
+        {
+            SceneManager.LoadScene(initialSceneName, LoadSceneMode.Single);
+        }
+    }
+}
